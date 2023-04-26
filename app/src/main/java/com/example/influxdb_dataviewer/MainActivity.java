4b07c0207d84441161b8781fc5eee2243b95238e
unknow0123456789 <nguyenphucbao832002@gmail.com>
2023-04-27 00:51:18 +0700

    0.1 - Undone Navigation bar Design Basic Query Function

--- app/src/main/java/com/example/influxdb_dataviewer/MainActivity.java
+++ app/src/main/java/com/example/influxdb_dataviewer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.influxdb_dataviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
@@ -23,28 +28,55 @@ public class MainActivity extends AppCompatActivity {
         String bucket = "unknowndb";
         String org = "UbuntuVM";
         String StartSampleTime="-1h";
+        ImageButton RefreshButton=findViewById(R.id.Refresh_BTN);
+        RefreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InfluxDBClient client = InfluxDBClientFactory.create("http://192.168.1.11:3004", token.toCharArray());
+                ReceiveDataFromInfluxDB(client, bucket, org, StartSampleTime, new CustomResponse() {
+                    @Override
+                    public void OnResponse(Object obj) {
 
-        InfluxDBClient client = InfluxDBClientFactory.create("http://192.168.1.11:3004", token.toCharArray());
-        String query = "from(bucket: \""+bucket+"\") |> range(start: "+StartSampleTime+")";
-
+                    }
+                });
+            }
+        });
+    }
+    public void ReceiveDataFromInfluxDB(InfluxDBClient currentClient, String BucketName, String ORG, String SampleFrom,CustomResponse CR)
+    {
         new Thread()
         {
             public void run()
             {
                 try {
-                    List<FluxTable> tables = client.getQueryApi().query(query, org);
+                    String query = "from(bucket: \""+BucketName+"\") |> range(start: "+SampleFrom+")";
+                    List<FluxTable> tables = currentClient.getQueryApi().query(query, ORG);
                     for (FluxTable table : tables) {
-                        Log.d("testTables", table.toString());
+                        Log.d("ReceivedTables", table.toString());
                         for (FluxRecord record : table.getRecords()) {
-                            Log.d("testQuery", record.getMeasurement()+" : "+record.getValue().toString());
+                            Log.d("ReceivedQuery", record.getMeasurement()+" : "+record.getValue().toString());
                         }
                     }
+                    CR.OnResponse(tables);
                 }
                 catch (Exception ex)
                 {
-                    
+                    Log.e("Query_Exception", ex.getMessage());
                 }
             }
         }.start();
+    }
+    public void SetFragmentToFocus(Fragment theFragment)
+    {
+        try {
+            FragmentManager fragmentManager=getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.FragmentHolder,theFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .commit();
+        }
+        catch (Exception ex)
+        {
+            Log.d("Attach Fragment Failed",ex.getMessage());
+        }
     }
 }
